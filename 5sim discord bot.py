589cf9148e83fbb2a38887423af065a52e844633
bonx <61289944+bonxz@users.noreply.github.com>
2022-04-16 20:03:59 +0200

    bot activity status

--- 5sim discord bot.py
+++ 5sim discord bot.py
@@ -256,6 +256,12 @@ async def help(ctx):
     embed.timestamp = datetime.utcnow()
     await ctx.send(embed=embed)
 
+   
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord. Activity(type=discord.ActivityType.listening, name='5sim.net'))
     
+    print('Connected to bot: {}'.format(bot.user.name))
+    print('Bot ID: {}'.format(bot.user.id))   
     
 bot.run('bot token here')
